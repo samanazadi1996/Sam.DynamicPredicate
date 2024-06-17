d8eabe826734089b87ee2ce4221f7b992c642ec7
saman azadi <samanazadi1996@gmail.com>
2024-06-17 06:39:19 +0330

    add tests

--- Source/Sam.DynamicPredicateTests/PersonFilterTests.cs
+++ Source/Sam.DynamicPredicateTests/PersonFilterTests.cs
@@ -7,17 +7,18 @@ namespace Sam.DynamicPredicateTests
         // Sample data for testing
         private readonly IEnumerable<Person> people = new List<Person>
         {
-            new Person { Id=1, Name = "John", Age = 25 },
-            new Person { Id=2, Name = "Jane", Age = 10 },
-            new Person { Id=3, Name = "Jake", Age = 20 },
-            new Person { Id=4, Name = "Jill", Age = 30 }
+            new Person { Id=1, Name = "John", Age = 25 ,Code="1"},
+            new Person { Id=2, Name = "Jane", Age = 10 ,Code="1"},
+            new Person { Id=3, Name = "Jake", Age = 20 ,Code="1"},
+            new Person { Id=4, Name = "Jill", Age = 30 },
+            new Person { Id=5, Name = null, Age = 40 , Code = "1"}
         };
 
         [Fact]
         public void Test_FilterByComplexCondition()
         {
             // Arrange
-            var predicate = "Name == \"Jake\" && Id >= 2 && (Age == 10 || Age == 20)";
+            var predicate = "Name == \"Jake\" && Id >= 2 && (Age == 10 || Age == 10000 || Age == 20)";
 
             // Act
             var filteredPeople = people.Where(predicate).ToList();
@@ -27,22 +28,6 @@ namespace Sam.DynamicPredicateTests
             Assert.Equal("Jake", filteredPeople[0].Name);
             Assert.Equal(20, filteredPeople[0].Age);
         }
-
-        [Fact]
-        public void Test_FilterByNameStartsWith()
-        {
-            // Arrange
-            var predicate = "Name StartsWith \"Ja\"";
-
-            // Act
-            var filteredPeople = people.Where(predicate).ToList();
-
-            // Assert
-            Assert.Equal(2, filteredPeople.Count);
-            Assert.Equal("Jane", filteredPeople[0].Name);
-            Assert.Equal("Jake", filteredPeople[1].Name);
-        }
-
 
         [Fact]
         public void Test_FilterByNonExistingName()
@@ -72,12 +57,86 @@ namespace Sam.DynamicPredicateTests
             Assert.Contains(filteredPeople, p => p.Name == "Jake");
             Assert.Contains(filteredPeople, p => p.Name == "Jill");
         }
+
+        [Fact]
+        public void Test_FilterByCodeIsNull()
+        {
+            // Arrange
+            var predicate = "Code Is null";
+
+            // Act
+            var filteredPeople = people.Where(predicate).ToList();
+
+            // Assert
+            Assert.Single(filteredPeople);
+            Assert.Contains(filteredPeople, p => p.Name == "Jill");
+        }
+
+        [Fact]
+        public void Test_FilterByNameIsNull()
+        {
+            // Arrange
+            var predicate = "Name Is null";
+
+            // Act
+            var filteredPeople = people.Where(predicate).ToList();
+
+            // Assert
+            Assert.Single(filteredPeople);
+            Assert.Null(filteredPeople[0].Name);
+            Assert.Equal(40, filteredPeople[0].Age);
+        }
+
+        [Fact]
+        public void Test_FilterByMultipleConditions()
+        {
+            // Arrange
+            var predicate = "Age >= 20 && (Name == \"John\" || Name == \"Jake\")";
+
+            // Act
+            var filteredPeople = people.Where(predicate).ToList();
+
+            // Assert
+            Assert.Equal(2, filteredPeople.Count);
+            Assert.Contains(filteredPeople, p => p.Name == "John");
+            Assert.Contains(filteredPeople, p => p.Name == "Jake");
+        }
+
+        [Fact]
+        public void Test_FilterByGreaterThan()
+        {
+            // Arrange
+            var predicate = "Age > 25";
+
+            // Act
+            var filteredPeople = people.Where(predicate).ToList();
+
+            // Assert
+            Assert.Equal(2, filteredPeople.Count);
+            Assert.Contains(filteredPeople, p => p.Name == "Jill");
+            Assert.Contains(filteredPeople, p => p.Age == 40);
+        }
+
+        [Fact]
+        public void Test_FilterByLessThan()
+        {
+            // Arrange
+            var predicate = "Age < 20";
+
+            // Act
+            var filteredPeople = people.Where(predicate).ToList();
+
+            // Assert
+            Assert.Single(filteredPeople);
+            Assert.Equal("Jane", filteredPeople[0].Name);
+        }
     }
 
     public class Person
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Code { get; set; }
         public int Age { get; set; }
     }
 }
